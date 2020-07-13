0995ef8ea42cf457b6db578d45a0625317145f6e
Luis Eduardo Craizer <lecraizer@tecgraf.puc-rio.br>
2020-07-13 10:39:32 -0300

    changes in final document and in preplotter module

--- previsualization/preplotter.py
+++ previsualization/preplotter.py
@@ -34,10 +34,10 @@ class PreVisualizer():
         Plot n pairs of regular and pneumonia x-ray images.
         """
         for i in range(0,5):
-            imagep1 = cv2.imread(self.DIR+"PNEUMONIA/"+self.PNEU_DIR[i])
-            imagep1 = skimage.transform.resize(imagep1, (150, 150, 3), mode = 'reflect')
             imagen1 = cv2.imread(self.DIR+"NORMAL/"+self.REG_DIR[i])
             imagen1 = skimage.transform.resize(imagen1, (150, 150, 3))
+            imagep1 = cv2.imread(self.DIR+"PNEUMONIA/"+self.PNEU_DIR[i])
+            imagep1 = skimage.transform.resize(imagep1, (150, 150, 3), mode = 'reflect')
             pair = np.concatenate((imagen1, imagep1), axis=1)
             plt.figure(figsize=(10,5))
             plt.imshow(pair)
